23a03938c31a92cfc0163de26cb4bef3173e8ed5
muro <muro.bakuridze@gmail.com>
2024-07-11 14:18:41 +0400

    Brand Links

--- resources/views/modules/companies/brands.blade.php
+++ resources/views/modules/companies/brands.blade.php
@@ -12,14 +12,20 @@
 			@if ($activeBrand->brands->isNotEmpty())
 				@foreach ($activeBrand->brands as $brand)
 					@if (file_exists(public_path('storage/images/modules/companies/89/' . $brand->id . '.png')))
-						<div class="col-xxl-2 col-lg-3 col-md-4 col-sm-6 p-2">	
-							<div class="brands__box text-center p-3">
-								<div>
-									<img class="mb-2" src="{{ asset('storage/images/modules/companies/89/' . $brand->id . '.png') }}" alt="{{ $brand->title }}">
-								</div>
+						<div class="col-xxl-2 col-lg-3 col-md-4 col-sm-6 p-2">
+							@if($brand->link)
+								<a href="{{ $brand->link }}">
+							@endif
+									<div class="brands__box text-center p-3">
+										<div>
+											<img class="mb-2" src="{{ asset('storage/images/modules/companies/89/' . $brand->id . '.png') }}" alt="{{ $brand->title }}">
+										</div>
 
-								<h3 class="brands__small_title">{{ $brand->title }}</h3>
-							</div>
+										<h3 class="brands__small_title">{{ $brand->title }}</h3>
+									</div>
+							@if($brand->link)
+								</a>
+							@endif
 						</div>
 					@endif
 				@endforeach
