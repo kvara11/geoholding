f9bda1701719da4617331592ede5f17fbde37767
Vaso Khachidze <khachidzev239@gmail.com>
2024-08-06 15:36:21 +0400

    change bsw footer

--- resources/views/modules/footer/basic.blade.php
+++ resources/views/modules/footer/basic.blade.php
@@ -270,8 +270,7 @@
                             p-2
                             text-lg-start
                             text-center">
-                    {{--  {{ config('constants.year_of_site_creation') }}  --}}
-                    © {{ __('bsw.copyright') }} 
+                    © {{ config('constants.year_of_site_creation') }} {{ __('bsw.copyright') }} 
                 </div>
 
                 <div class="col-lg-4
